ad4573e6308c47c3976c1ef71c598c1463a08627
Fabian David Schmidt <fabian.david.schmidt@gmail.com>
2021-11-14 20:34:24 +0100

    feat: wip wip wip wip

--- lua/telescope/_extensions/file_browser.lua
+++ lua/telescope/_extensions/file_browser.lua
@@ -1,6 +1,6 @@
 ---@brief [[
 --- Telescope-file-browser.nvim is an extension for telescope.nvim. It helps you efficiently
---- create, delete, rename, or move files powered by navigation from telescope.nvim
+--- create, delete, rename, or move files powered by navigation from telescope.nvim.
 ---
 --- The `telescope-file-browser` is setup via the `telescope` extension interface.<br>
 --- You can manage the settings for the `telescope-file-browser` analogous to how you
@@ -22,6 +22,15 @@
 --- telescope.load_extension "file_browser"
 --- </code>
 ---
+--- The extension exports { "file_browser", "picker", "actions", "finder" } modules via:
+--- </code>
+--- require "telescope".extensions.file_browser
+--- </code>
+--- In particular:
+--- - `file_browser`: constitutes the main picker of the extension
+--- - `picker`: unconfigured equivalent of `file_browser` defaulting to extensiond defaults
+--- - `actions`: extension actions make accessible for remapping and custom usage
+--- - `finder`: low-level finders -- if you need to access them you know what you are doing
 --- <pre>
 --- To find out more:
 --- https://github.com/nvim-telescope/telescope-file-browser.nvim
