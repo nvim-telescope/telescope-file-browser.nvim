df131aa5da5aa0b2169fa504b991f4bc9f4de3d0
fdschmidt93 <39233597+fdschmidt93@users.noreply.github.com>
2021-12-08 11:54:47 +0100

    fix: keymapping defaults and overrides (#6)
    This PR should largely fix telescope-file-browser mappings override. Some (primarily upstream) issues remain that will be addressed over at the core repo eventually.
    
    * fix: keymapping defaults and overrides
    
    * Consistent default keymappings between insert and normal mode
    * Fix being unable to fully override (unmap) default keymappings
    
    * [docgen] Update doc/telescope-file-browser.txt
    skip-checks: true
    
    Co-authored-by: Github Actions <actions@github>

--- lua/telescope/_extensions/file_browser/picker.lua
+++ lua/telescope/_extensions/file_browser/picker.lua
@@ -14,13 +14,10 @@
 --- </code>
 ---@brief ]]
 
-local action_state = require "telescope.actions.state"
-local action_set = require "telescope.actions.set"
 local pickers = require "telescope.pickers"
 local conf = require("telescope.config").values
 
 local fb_finder = require "telescope._extensions.file_browser.finders"
-local fb_actions = require "telescope._extensions.file_browser.actions"
 
 local Path = require "plenary.path"
 local os_sep = Path.path.sep
@@ -31,7 +28,7 @@ local fb_picker = {}
 --- List, create, delete, rename, or move files and folders of your cwd.
 --- Default keymaps in insert/normal mode:
 ---   - `<cr>`: opens the currently selected file, or navigates to the currently selected directory
----   - `<C-e>`: creates new file in current directory, creates new directory if the name contains a trailing '/'
+---   - `<C-e/e>`: creates new file in current directory, creates new directory if the name contains a trailing '/'
 ---     - Note: you can create files nested into several directories with `<C-e>`, i.e. `lua/telescope/init.lua` would
 ---       create the file `init.lua` inside of `lua/telescope` and will create the necessary folders (similar to how
 ---       `mkdir -p` would work) if they do not already exist
@@ -41,7 +38,7 @@ local fb_picker = {}
 ---   - `<C-y>/y`: copy multi selected file(s) or folder(s) recursively to current directory
 ---   - `<C-f>/f`: toggle between file and folder browser
 ---   - `<C-h>/h`: toggle hidden files
----   - `<C-d/dd>`: remove currently or multi selected file(s) or folder(s) recursively
+---   - `<C-d>/dd`: remove currently or multi selected file(s) or folder(s) recursively
 ---   - --/m`: move multi selected file(s) or folder(s) recursively to current directory in file browser
 ---@param opts table: options to pass to the picker
 ---@field path string: root dir to file_browse from (default: vim.loop.cwd())
@@ -63,53 +60,6 @@ fb_picker.file_browser = function(opts)
     finder = fb_finder.finder(opts),
     previewer = conf.file_previewer(opts),
     sorter = conf.file_sorter(opts),
-    -- TODO(fdschmidt93): discuss tami's suggestion
-    on_input_filter_cb = function(prompt)
-      if prompt:sub(-1, -1) == os_sep then
-        local prompt_bufnr = vim.api.nvim_get_current_buf()
-        if vim.bo[prompt_bufnr].filetype == "TelescopePrompt" then
-          local current_picker = action_state.get_current_picker(prompt_bufnr)
-          if current_picker.finder.files then
-            fb_actions.toggle_browser(prompt_bufnr, { reset_prompt = true })
-            current_picker:set_prompt(prompt:sub(1, -2))
-          end
-        end
-      end
-    end,
-    attach_mappings = function(prompt_bufnr, map)
-      action_set.select:replace_if(function()
-        -- test whether selected entry is directory
-        local selected_entry = action_state.get_selected_entry()
-        return selected_entry and selected_entry.path:sub(-1, -1) == os_sep
-      end, function()
-        local path = vim.loop.fs_realpath(action_state.get_selected_entry().path)
-        local current_picker = action_state.get_current_picker(prompt_bufnr)
-        current_picker.results_border:change_title(Path:new(path):make_relative(cwd) .. os_sep)
-        local finder = current_picker.finder
-        finder.files = true
-        finder.path = path
-        current_picker:refresh(finder, { reset_prompt = true, multi = current_picker._multi })
-      end)
-      map("i", "<C-e>", fb_actions.create_file)
-      map("n", "<C-e>", fb_actions.create_file)
-      map("i", "<C-r>", fb_actions.rename_file)
-      map("n", "<C-r>", fb_actions.rename_file)
-      map("i", "<C-o>", fb_actions.open_file)
-      map("n", "<C-o>", fb_actions.open_file)
-      map("i", "<C-y>", fb_actions.copy_file)
-      map("n", "y", fb_actions.copy_file)
-      map("i", "<C-h>", fb_actions.toggle_hidden)
-      map("n", "<h>", fb_actions.toggle_hidden)
-      map("i", "<C-g>", fb_actions.goto_prev_dir)
-      map("n", "g", fb_actions.goto_prev_dir)
-      map("i", "<C-f>", fb_actions.toggle_browser)
-      map("n", "f", fb_actions.toggle_browser)
-      map("i", "<C-w>", fb_actions.goto_cwd)
-      map("n", "m", fb_actions.move_file)
-      map("n", "dd", fb_actions.remove_file)
-      map("i", "<C-d>", fb_actions.remove_file)
-      return true
-    end,
   }):find()
 end
 
