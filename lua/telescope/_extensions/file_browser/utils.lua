192f0cc9147e6112123b87d6503b4273ca157ffd
Brian Mayo <ioprotium@gmail.com>
2022-06-30 13:05:47 +0200

    feat: update finder prompt title if follow enabled

--- lua/telescope/_extensions/file_browser/utils.lua
+++ lua/telescope/_extensions/file_browser/utils.lua
@@ -9,8 +9,6 @@ local truncate = require("plenary.strings").truncate
 
 local fb_utils = {}
 
-local Path = require "plenary.path"
-
 fb_utils.is_dir = function(path)
   if Path.is_path(path) then
     return path:is_dir()
@@ -112,12 +110,11 @@ fb_utils.redraw_border_title = function(current_picker)
   local finder = current_picker.finder
   if current_picker.prompt_border then
     if finder.follow then
-      local parent = Path:new(finder.cwd):parent().filename
-      local new_title = Path:new(finder.path):make_relative(parent)
-      if parent == finder.path then
-        new_title = parent
+      local new_title = Path:new(finder.path):make_relative(finder.cwd)
+      if new_title == '.' then
+        -- show the folder name
+        new_title = vim.fn.expand('%:p:h:t')
       end
-
       current_picker.prompt_border:change_title(new_title)
     elseif not finder.prompt_title then
       local new_title = finder.files and "File Browser" or "Folder Browser"
